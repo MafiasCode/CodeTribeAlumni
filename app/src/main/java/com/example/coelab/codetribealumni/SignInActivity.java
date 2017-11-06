f0dd8445cdfada75c1b8d48784790e9f85bad26c
[pontshorichard20@gmail.com] <[pontshorichard20@gmail.com]>
2017-11-06 12:12:12 -0800

    PR commit

--- app/src/main/java/com/example/coelab/codetribealumni/SignInActivity.java
+++ app/src/main/java/com/example/coelab/codetribealumni/SignInActivity.java
@@ -1,17 +1,18 @@
 package com.example.coelab.codetribealumni;
 
 import android.app.ActionBar;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(getApplicationContext(),SignInActivity.class));
-        //
+        setContentView(R.layout.activity_sign_in);
+        //ActionBar actionBar = getActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    
 }
