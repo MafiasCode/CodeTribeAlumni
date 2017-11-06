6f58ac002f3fd47cd90a94e1fdf3d229a5970878
yanga2 <yongamatsele@gmail.com>
2017-11-06 00:35:21 -0800

    Created The Main xml Screen and coded the main screen buttons

--- app/src/main/java/com/example/coelab/codetribealumni/MainActivity.java
+++ app/src/main/java/com/example/coelab/codetribealumni/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.coelab.codetribealumni;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("");
         //
+    }
+
+    //code to intent to the next screen depending on the pressed button
+    public void sign_in(View view){
+
+        //Intent intent = new Intent(this,SignInActivity.class);
+        //startActivity(intent);
+    }
+
+    public void sign_up(View view){
+        //Intent intent = new Intent(this,SignUpActivity.class);
+        //startActivity(intent);
     }
 }
