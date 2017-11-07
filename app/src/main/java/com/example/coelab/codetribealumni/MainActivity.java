35083ad096919b4de1280afc84f21e4a3add4377
PontshoModisane <pontshorichard20@gmail.com>
2017-11-07 10:27:21 -0800

    commit

--- app/src/main/java/com/example/coelab/codetribealumni/MainActivity.java
+++ app/src/main/java/com/example/coelab/codetribealumni/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(getApplicationContext(),SignInActivity.class));
+        //startActivity(new Intent(getApplicationContext(),SignInActivity.class));
         //
+    }
+    public void sign_in(View view){
+
+        Intent intent = new Intent(this,SignInActivity.class);
+        startActivity(intent);
+    }
+
+    public void sign_up(View view){
+        //Intent intent = new Intent(this,SignUpActivity.class);
+        //startActivity(intent);
     }
 }
