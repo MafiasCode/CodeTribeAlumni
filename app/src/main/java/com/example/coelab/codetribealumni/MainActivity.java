79d9d3c59adb7d5d93844d1208063f0b30131a3e
yanga2 <yongamatsele@gmail.com>
2017-11-07 00:13:09 -0800

    did intent with the sign in screen

--- app/src/main/java/com/example/coelab/codetribealumni/MainActivity.java
+++ app/src/main/java/com/example/coelab/codetribealumni/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.coelab.codetribealumni;
 
-import android.content.Intent;
-import android.app.ActionBar;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
+        import android.content.Intent;
+        import android.app.ActionBar;
+        import android.content.Intent;
+        import android.support.v7.app.ActionBarDrawerToggle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("");
+
+
         //
     }
 
     //code to intent to the next screen depending on the pressed button
     public void sign_in(View view){
 
-        //Intent intent = new Intent(this,SignInActivity.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this,SignInActivity.class);
+        startActivity(intent);
     }
 
     public void sign_up(View view){
