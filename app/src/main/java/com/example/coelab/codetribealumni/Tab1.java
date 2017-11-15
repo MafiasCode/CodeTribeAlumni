097c07eae90f2133fa523c38c3bc0c2ded56ab1d
yanga2 <yongamatsele@gmail.com>
2017-11-15 05:52:33 -0800

    added about us activity and activities for accomplishments and career opportunities

--- app/src/main/java/com/example/coelab/codetribealumni/Tab1.java
+++ app/src/main/java/com/example/coelab/codetribealumni/Tab1.java
@@ -1,14 +1,19 @@
 package com.example.coelab.codetribealumni;
 
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,11 +25,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Laser on 11/13/2017.
  */
 
 public class Tab1 extends Fragment {
 
-
+ 
 }
